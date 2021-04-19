6b8d62dee16541047144db982a046263afb2289c
Pablo Antonio <pablo.barron@gmail.com>
2021-04-18 22:30:20 -0500

    complemento de las respuestas de cap01

--- cap01/SingleFileZoo.java
+++ cap01/SingleFileZoo.java
@@ -4,7 +4,7 @@ public class SingleFileZoo {
 
 
     public static void main(String... args){
-        uhho;
+        //uhho;   Si descomentas esta linea , se propagara la exception java.lang.Error por no esta declarada la variable local uhho.
         System.out.println("Single file "+args[0]);
     }
     
